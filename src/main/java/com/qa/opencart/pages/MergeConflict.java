cd7ebcc90b2858e95ab16123353afe7ef917e27b
pverma709 <poojaver709@gmail.com>
2024-10-06 17:32:03 -0700

    Update MergeConflict.java

--- src/main/java/com/qa/opencart/pages/MergeConflict.java
+++ src/main/java/com/qa/opencart/pages/MergeConflict.java
@@ -4,9 +4,9 @@ public class MergeConflict {
 
 	public static void main(String[] args) {
 
-
+		double d = 1.2;		
 		System.out.println("learning merge conflict---NOT PV someone else");
-
+;
 	}
 
 }
