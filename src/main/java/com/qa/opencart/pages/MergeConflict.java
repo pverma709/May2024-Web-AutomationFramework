6c8c6e83bce2dc31e4985061c954ffbee369482f
pverma709 <poojaver709@gmail.com>
2024-10-06 17:37:24 -0700

    commiting merge conflict file

--- src/main/java/com/qa/opencart/pages/MergeConflict.java
+++ src/main/java/com/qa/opencart/pages/MergeConflict.java
@@ -3,8 +3,8 @@ package com.qa.opencart.pages;
 public class MergeConflict {
 
 	public static void main(String[] args) {
-
-
+		
+		int i = 4;
 		System.out.println("learning merge conflict---NOT PV someone else");
 
 	}
