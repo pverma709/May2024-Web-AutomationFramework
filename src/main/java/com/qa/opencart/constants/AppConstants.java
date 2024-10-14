e66b5127ee6eeba09f4866fc761b9a8d50a108d2
pverma709 <poojaver709@gmail.com>
2024-10-13 22:14:08 -0700

    fixed accountPageTitle

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -15,7 +15,7 @@ public class AppConstants {
 	
 	public static final int DEFAULT_LONG_TIME_OUT = 20;
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
 	
